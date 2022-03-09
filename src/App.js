fa3be617634db42d522073b72dd3eb3d4fa55074
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-09 11:31:19 +0300

    Add Header to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
