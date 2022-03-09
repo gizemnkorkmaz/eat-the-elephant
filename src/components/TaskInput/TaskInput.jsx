bf19068b403ead2b2d10a29e62a3532be4c4d3eb
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-09 22:38:05 +0300

    Remove return statement in TaskInput

--- src/components/TaskInput/TaskInput.jsx
+++ src/components/TaskInput/TaskInput.jsx
@@ -3,21 +3,19 @@ import styles from "./TaskInput.module.css";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
 
-const TaskInput = ({ task, setTask }) => {
-  return (
-    <div className={styles.InputArea}>
-      <input
-        type="text"
-        className={styles.TaskInput}
-        placeholder="Enter your task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <Button onClick={() => setTask("")}>
-        <Icon icon="plus" size={20} />
-      </Button>
-    </div>
-  );
-};
+const TaskInput = ({ task, setTask }) => (
+  <div className={styles.InputArea}>
+    <input
+      type="text"
+      className={styles.TaskInput}
+      placeholder="Enter your task"
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+    />
+    <Button onClick={() => setTask("")}>
+      <Icon icon="plus" size={20} />
+    </Button>
+  </div>
+);
 
 export default TaskInput;
