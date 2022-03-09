9819bbbe35905ed703203e6ac54ff297342dc1dd
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-09 22:17:36 +0300

    Add task state and TaskInput to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "./components/Header/Header";
+import TaskInput from "./components/TaskInput/TaskInput";
 
 function App() {
+  const [task, setTask] = useState("");
+
   return (
     <div className="App">
       <Header />
+      <TaskInput task={task} setTask={setTask} />
     </div>
   );
 }
