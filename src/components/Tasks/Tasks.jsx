eea91f40e99dc0503f4d35e73ad4831f4dfefb1c
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-11 23:12:20 +0300

    Add deleteTask function, delete icon and unchecked checkbox icon to Tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,5 +1,7 @@
 import styles from "./Tasks.module.css";
+
 import Task from "../Task/Task";
+import Icon from "../Icon/Icon";
 
 const Tasks = ({ tasks, setTasks }) => {
   const toggleIsDone = (id) => {
@@ -10,11 +12,34 @@ const Tasks = ({ tasks, setTasks }) => {
     );
   };
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className={styles.Tasks}>
       <h1 className={styles.TasksHeader}>Today</h1>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onClick={() => toggleIsDone(task.id)} />
+        <Task key={task.id} task={task}>
+          <div onClick={() => toggleIsDone(task.id)}>
+            <Icon
+              icon={task.isDone ? "checkbox-checked" : "checkbox-unchecked"}
+              size={20}
+              className={styles.Icon}
+            />
+            <span className={task.isDone ? styles.TaskDone : ""}>
+              {task.name}
+            </span>
+          </div>
+          <div>
+            <Icon
+              icon="bin"
+              size={20}
+              className={styles.Icon}
+              onClick={() => deleteTask(task.id)}
+            />
+          </div>
+        </Task>
       ))}
     </div>
   );
