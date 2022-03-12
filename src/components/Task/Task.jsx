47a381869bf08dda0cfb98593c130c89e728d0ee
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-12 13:45:26 +0300

    Relocate TaskItem render part from Tasks to Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,8 +1,46 @@
+import cx from "classnames";
 import styles from "./Task.module.css";
 
-const Task = ({ children, ...props }) => (
-  <div className={styles.TaskItem} {...props}>
-    {children}
+import Icon from "../Icon/Icon";
+
+const Task = ({ task, toggleIsDone, deleteTask, editTask, handleEdit }) => (
+  <div className={styles.TaskItem}>
+    {task.editTask ? (
+      <input
+        type="text"
+        className={styles.EditInput}
+        value={task.name}
+        onChange={(e) => handleEdit(e, task.id)}
+      />
+    ) : (
+      <>
+        <Icon
+          icon={task.isDone ? "checkbox-checked" : "checkbox-unchecked"}
+          size={20}
+          className={styles.Icon}
+          onClick={() => toggleIsDone(task.id)}
+        />
+        <p
+          className={cx(styles.Task, {
+            [styles.TaskDone]: task.isDone,
+          })}
+        >
+          {task.name}
+        </p>
+        <Icon
+          icon="bin"
+          size={20}
+          className={styles.Icon}
+          onClick={() => deleteTask(task.id)}
+        />
+      </>
+    )}
+    <Icon
+      icon={task.editTask ? "checkbox-checked" : "pencil"}
+      size={18}
+      className={styles.Icon}
+      onClick={() => editTask(task.id)}
+    />
   </div>
 );
 
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,7 +1,6 @@
 import styles from "./Tasks.module.css";
 
 import Task from "../Task/Task";
-import Icon from "../Icon/Icon";
 
 const Tasks = ({ tasks, setTasks }) => {
   const toggleIsDone = (id) => {
@@ -36,42 +35,14 @@ const Tasks = ({ tasks, setTasks }) => {
     <div className={styles.Tasks}>
       <h1 className={styles.TasksHeader}>Today</h1>
       {tasks.map((task) => (
-        <Task key={task.id}>
-          <span className={task.isDone ? styles.TaskDone : ""}>
-            {task.editTask ? (
-              <input
-                type="text"
-                className={styles.EditInput}
-                value={task.name}
-                onChange={(e) => handleEdit(e, task.id)}
-              />
-            ) : (
-              <div>
-                <Icon
-                  icon={task.isDone ? "checkbox-checked" : "checkbox-unchecked"}
-                  size={20}
-                  className={styles.Icon}
-                  onClick={() => toggleIsDone(task.id)}
-                />
-                {task.name}
-              </div>
-            )}
-          </span>
-          <div>
-            <Icon
-              icon="bin"
-              size={20}
-              className={styles.Icon}
-              onClick={() => deleteTask(task.id)}
-            />
-            <Icon
-              icon={task.editTask ? "checkbox-checked" : "pencil"}
-              size={18}
-              className={styles.Icon}
-              onClick={() => editTask(task.id)}
-            />
-          </div>
-        </Task>
+        <Task
+          key={task.id}
+          task={task}
+          toggleIsDone={toggleIsDone}
+          deleteTask={deleteTask}
+          editTask={editTask}
+          handleEdit={handleEdit}
+        />
       ))}
     </div>
   );
