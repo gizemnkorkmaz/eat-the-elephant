81299f947bc49410288a6a1c7869c90ede028a1e
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-14 01:59:07 +0300

    Add Icon styles

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,6 +1,10 @@
+import styles from "./Icon.module.css";
+
 import IcoMoon from "react-icomoon";
 const iconSet = require("./selection.json");
 
-const Icon = (props) => <IcoMoon iconSet={iconSet} {...props} />;
+const Icon = (props) => (
+  <IcoMoon iconSet={iconSet} className={styles.Icon} {...props} />
+);
 
 export default Icon;
