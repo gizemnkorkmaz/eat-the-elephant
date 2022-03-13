3d9b3849a54bf6e9901599b0233dd1156c7d149e
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-14 02:09:44 +0300

    Remove task related functions from Tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -3,46 +3,11 @@ import styles from "./Tasks.module.css";
 import Task from "../Task/Task";
 
 const Tasks = ({ tasks, setTasks }) => {
-  const toggleIsDone = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, isDone: !task.isDone } : task
-      )
-    );
-  };
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const editTask = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, editTask: !task.editTask } : task
-      )
-    );
-  };
-
-  const handleEdit = (e, id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, name: e.target.value } : task
-      )
-    );
-  };
-
   return (
     <div className={styles.Tasks}>
       <h1 className={styles.TasksHeader}>Today</h1>
       {tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          toggleIsDone={toggleIsDone}
-          deleteTask={deleteTask}
-          editTask={editTask}
-          handleEdit={handleEdit}
-        />
+        <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
       ))}
     </div>
   );
