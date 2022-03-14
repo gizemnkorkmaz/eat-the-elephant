234e95adec1844e4f14d237792a2719b13466cd2
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-14 22:45:47 +0300

    Add editTaskId and setEditTaskId props to Tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -2,12 +2,19 @@ import styles from "./Tasks.module.css";
 
 import Task from "../Task/Task";
 
-const Tasks = ({ tasks, setTasks }) => {
+const Tasks = ({ tasks, setTasks, editTaskId, setEditTaskId }) => {
   return (
     <div className={styles.Tasks}>
       <h1 className={styles.TasksHeader}>Today</h1>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        <Task
+          key={task.id}
+          task={task}
+          tasks={tasks}
+          setTasks={setTasks}
+          editTaskId={editTaskId}
+          setEditTaskId={setEditTaskId}
+        />
       ))}
     </div>
   );
