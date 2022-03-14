d071c21a345e477d8f1a04b590415568e6a37d99
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-14 22:44:01 +0300

    Refactor saveEdit function in EditTask

--- src/components/EditTask/EditTask.jsx
+++ src/components/EditTask/EditTask.jsx
@@ -4,23 +4,20 @@ import styles from "./EditTask.module.css";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
 
-const EditTask = ({ task, tasks, setTasks }) => {
+const EditTask = ({ task, tasks, setTasks, editTaskId, setEditTaskId }) => {
   const [editedTask, setEditedTask] = useState(task);
 
   const handleEdit = (e) => {
     setEditedTask({ ...task, name: e.target.value });
   };
 
-  const saveEdit = (e, id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id
-          ? e.target.innerText === "Save"
-            ? { ...editedTask, isEditActive: false }
-            : { ...task, isEditActive: false }
-          : task
-      )
-    );
+  const saveEdit = () => {
+    setEditTaskId(null);
+    setTasks(tasks.map((task) => (task.id === editTaskId ? editedTask : task)));
+  };
+
+  const cancelEdit = () => {
+    setEditTaskId(null);
   };
 
   return (
@@ -30,13 +27,13 @@ const EditTask = ({ task, tasks, setTasks }) => {
           type="text"
           value={editedTask.name}
           className={styles.Input}
-          onChange={(e) => handleEdit(e, task.id)}
+          onChange={handleEdit}
         />
         <Icon icon="calendar" size={20} />
       </div>
       <div className={styles.Buttons}>
-        <Button onClick={(e) => saveEdit(e, task.id)}>Save</Button>
-        <Button onClick={(e) => saveEdit(e, task.id)}>Cancel</Button>
+        <Button onClick={saveEdit}>Save</Button>
+        <Button onClick={cancelEdit}>Cancel</Button>
       </div>
     </div>
   );
