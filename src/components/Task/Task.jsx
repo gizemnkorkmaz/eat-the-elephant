37b7f968ab33bbbedf52fc8a9c5a57829cb345ac
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-14 22:45:06 +0300

    Add currentTaskId for task.id and refactor functions accordingly

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -4,50 +4,58 @@ import styles from "./Task.module.css";
 import Icon from "../Icon/Icon";
 import EditTask from "../EditTask/EditTask";
 
-const Task = ({ task, tasks, setTasks }) => {
-  const toggleIsDone = (id) => {
+const Task = ({ task, tasks, setTasks, editTaskId, setEditTaskId }) => {
+  const currentTaskId = task.id;
+
+  const toggleIsDone = () => {
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, isDone: !task.isDone } : task
+        task.id === currentTaskId ? { ...task, isDone: !task.isDone } : task
       )
     );
   };
 
-  const activateEdit = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, isEditActive: true } : task
-      )
-    );
+  const activateEdit = () => {
+    setEditTaskId(task.id);
   };
 
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+  const deleteTask = () => {
+    setTasks(tasks.filter((task) => task.id !== currentTaskId));
   };
 
   return (
-    <div className={styles.TaskItem}>
-      {task.isEditActive ? (
-        <EditTask task={task} tasks={tasks} setTasks={setTasks} />
+    <>
+      {editTaskId === task.id ? (
+        <EditTask
+          task={task}
+          tasks={tasks}
+          setTasks={setTasks}
+          editTaskId={editTaskId}
+          setEditTaskId={setEditTaskId}
+        />
       ) : (
-        <>
-          <Icon
-            icon={task.isDone ? "checkbox-checked" : "checkbox-unchecked"}
-            size={20}
-            onClick={() => toggleIsDone(task.id)}
-          />
-          <p
-            className={cx(styles.Task, {
-              [styles.TaskDone]: task.isDone,
-            })}
-          >
-            {task.name}
-          </p>
-          <Icon icon="pencil" size={20} onClick={() => activateEdit(task.id)} />
-          <Icon icon="bin" size={20} onClick={() => deleteTask(task.id)} />
-        </>
+        <div className={styles.TaskWrapper}>
+          <div className={styles.TaskItem}>
+            <Icon
+              icon={task.isDone ? "checkbox-checked" : "checkbox-unchecked"}
+              size={20}
+              onClick={toggleIsDone}
+            />
+            <p
+              className={cx(styles.TaskName, {
+                [styles.TaskDone]: task.isDone,
+              })}
+            >
+              {task.name}
+            </p>
+          </div>
+          <div className={styles.Buttons}>
+            <Icon icon="pencil" size={20} onClick={activateEdit} />
+            <Icon icon="bin" size={20} onClick={deleteTask} />
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 export default Task;
