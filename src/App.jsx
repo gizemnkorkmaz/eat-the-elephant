c88fb0ecf4da0a53eb66159159337c3f6ce102cc
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-16 22:16:04 +0300

    Add tasksContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState, createContext } from "react";
-
 import "./App.css";
+
 import Header from "./components/Header/Header";
 import TaskInput from "./components/TaskInput/TaskInput";
 import Tasks from "./components/Tasks/Tasks";
 
-export const TasksContext = createContext();
-export const EditTaskIdContext = createContext();
+import { TasksProvider } from "./context/tasksContext";
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [editTaskId, setEditTaskId] = useState(null);
-
-  return (
-    <div className="App">
-      <Header />
-      <div className="TaskBoard">
-        <section className="TaskMenu">My Lists</section>
-        <section className="TaskListArea">
-          <TasksContext.Provider value={[tasks, setTasks]}>
-            <TaskInput />
-            {tasks.length > 0 && (
-              <EditTaskIdContext.Provider value={[editTaskId, setEditTaskId]}>
-                <Tasks />
-              </EditTaskIdContext.Provider>
-            )}
-          </TasksContext.Provider>
-        </section>
-      </div>
+const App = () => (
+  <div className="App">
+    <Header />
+    <div className="TaskBoard">
+      <section className="TaskMenu">My Lists</section>
+      <section className="TaskListArea">
+        <TasksProvider>
+          <TaskInput />
+          <Tasks />
+        </TasksProvider>
+      </section>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
