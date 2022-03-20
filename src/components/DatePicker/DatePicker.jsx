b9b6129b880dc88de24d86067d2d1e4fac9a0b98
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-20 21:56:28 +0300

    Remove taskDate context and Add selected and onChange props to DatePicker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,27 +1,20 @@
 import styles from "./DatePicker.module.css";
 import "react-datepicker/dist/react-datepicker.css";
 
-import { useContext } from "react";
-import { TasksContext } from "../../context/tasksContext";
-
 import DatePicker from "react-datepicker";
 import Icon from "../Icon/Icon";
 
-const Datepicker = () => {
-  const { taskDate, setTaskDate } = useContext(TasksContext);
-
-  return (
-    <label className={styles.CalendarWrapper}>
-      <DatePicker
-        selected={taskDate}
-        onChange={(e) => setTaskDate(e)}
-        className={styles.Calendar}
-        minDate={new Date()}
-        dateFormat="d MMMM"
-      />
-      <Icon icon="calendar" size={18} className={styles.CalendarIcon} />
-    </label>
-  );
-};
+const Datepicker = ({ selected, onChange }) => (
+  <label className={styles.CalendarWrapper}>
+    <DatePicker
+      selected={selected ? new Date(selected) : new Date()}
+      onChange={onChange}
+      className={styles.Calendar}
+      minDate={new Date()}
+      dateFormat="d MMMM"
+    />
+    <Icon icon="calendar" size={18} className={styles.CalendarIcon} />
+  </label>
+);
 
 export default Datepicker;
