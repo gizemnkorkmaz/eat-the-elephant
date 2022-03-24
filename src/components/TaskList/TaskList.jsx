317e0c5f4bb03869cc167eb4c19229de8aa6321a
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-25 01:05:55 +0300

    Update allTaskList naming in TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,10 +1,14 @@
 import Tasks from "../Tasks/Tasks";
 
 const TaskList = ({ taskLists, selectedList }) => {
-  const allTasks = Object.values(taskLists).filter((list) => list !== "Inbox");
+  const allTaskLists = Object.values(taskLists).filter(
+    (list) => list !== "Inbox"
+  );
 
   return selectedList === "Inbox" ? (
-    allTasks.map((listItem) => <Tasks key={listItem} selectedList={listItem} />)
+    allTaskLists.map((listItem) => (
+      <Tasks key={listItem} selectedList={listItem} />
+    ))
   ) : (
     <Tasks selectedList={selectedList} />
   );
