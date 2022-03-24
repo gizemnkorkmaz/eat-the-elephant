2122b19cfa38a8b8c8249851127e1d58fa7c5165
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-24 14:58:57 +0300

    Update mapped task list as selectedTaskList in Tasks

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,17 +1,19 @@
+import styles from "./Tasks.module.css";
+
 import { useContext } from "react";
 import { TasksContext } from "../../context/tasksContext";
-
-import styles from "./Tasks.module.css";
+import sortByDate from "../../utils/sortByDate";
 
 import Task from "../Task/Task";
 
-const Tasks = () => {
+const Tasks = ({ selectedList }) => {
   const { tasks } = useContext(TasksContext);
+  const selectedTaskList = sortByDate(selectedList, tasks);
 
   return (
     <div className={styles.Tasks}>
-      <h1 className={styles.TasksHeader}>Today</h1>
-      {tasks.map((task) => (
+      <h1 className={styles.TasksHeader}>{selectedList}</h1>
+      {selectedTaskList.map((task) => (
         <Task key={task.id} task={task} />
       ))}
     </div>
