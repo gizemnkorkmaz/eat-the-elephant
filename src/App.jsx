2248b395127f7d098d69b5eebbe96533351bccf3
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-24 14:58:23 +0300

    Add selectedList state and selectedTaskList function to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import "./App.css";
 
-import Header from "./components/Header/Header";
-import TaskInput from "./components/TaskInput/TaskInput";
-import Tasks from "./components/Tasks/Tasks";
-
+import { useState } from "react";
 import { TasksProvider } from "./context/tasksContext";
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <div className="TaskBoard">
-      <section className="TaskMenu">My Lists</section>
-      <section className="TaskListArea">
-        <TasksProvider>
-          <TaskInput />
-          <Tasks />
-        </TasksProvider>
-      </section>
+import Header from "./components/Header/Header";
+import TaskInput from "./components/TaskInput/TaskInput";
+import TaskMenu from "./components/TaskMenu/TaskMenu";
+import Tasks from "./components/Tasks/Tasks";
+
+const App = () => {
+  const [selectedList, setSelectedList] = useState("Today");
+
+  const selectTaskList = (selectedList) => {
+    const taskLists = {
+      today: "Today",
+      tomorrow: "Tomorrow",
+      thisWeek: "This Week",
+      allTasks: "All Tasks",
+    };
+
+    setSelectedList(taskLists[selectedList]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="TaskBoard">
+        <section className="TaskMenu">
+          <TaskMenu
+            selectTaskList={selectTaskList}
+            selectedList={selectedList}
+          />
+        </section>
+        <section className="TaskListArea">
+          <TasksProvider>
+            <TaskInput />
+            <Tasks selectedList={selectedList} />
+          </TasksProvider>
+        </section>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
