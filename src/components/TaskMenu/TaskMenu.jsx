3e3e06ae259b14b523dbd47dadf2c8c7cd04a833
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-03-24 15:01:05 +0300

    Add TaskMenu component

---
+++ src/components/TaskMenu/TaskMenu.jsx
@@ -0,0 +1,32 @@
+import styles from "./TaskMenu.module.css";
+import cx from "classnames";
+
+import Icon from "../Icon/Icon";
+
+const TaskMenu = ({ selectTaskList, selectedList }) => {
+  const taskLists = [
+    { value: "today", label: "Today", icon: "star-empty" },
+    { value: "tomorrow", label: "Tomorrow", icon: "pushpin" },
+    { value: "thisWeek", label: "This Week", icon: "hour-glass" },
+    { value: "allTasks", label: "All Tasks", icon: "rocket" },
+  ];
+
+  return (
+    <div className={styles.TaskMenu}>
+      {taskLists.map(({ value, label, icon }) => (
+        <div
+          className={cx(styles.TaskList, {
+            [styles.Active]: selectedList === label,
+          })}
+          key={value}
+          onClick={() => selectTaskList(value)}
+        >
+          <Icon icon={icon} size={15} />
+          {label}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TaskMenu;
