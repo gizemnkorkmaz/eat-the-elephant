1e15c50afc12863a0759d5537e6ed065730bb3d8
Gizem <66412137+gizemnkorkmaz@users.noreply.github.com>
2022-04-04 09:05:07 +0300

    Add sortedTasks array (#16)
    * Update styles
    
    * Add plus icon to selection.json
    
    * Add EmptyTaskList to TaskList.jsx
    
    * Update return value in EmptyTaskList.jsx
    
    * Add add icon to TaskInput.jsx
    
    * Remove Header and EmptyTaskList from App
    
    * Update styles
    
    * Remove Header
    
    * Update styles
    
    * Fix typo
    
    * Update styles
    
    * Update styles
    
    * Update styles
    
    * Update styles
    
    * Fix responsive issues
    
    * Add max-width
    
    * Add color to trashbin icon in selection.json
    
    * Update header tag in Task.jsx
    
    * Update return structure of TaskInput
    
    * Update header tag in Task.jsx
    
    * Update general styles
    
    * Remove custom color value from selection.json
    
    * Add delete button className in Task.jsx
    
    * Remove margin in EditTask styles
    
    * Remove margin and add delete button styles to Task styles
    
    * Remove the border of last child in Task styles
    
    * Aligned buttons and removed padding in Task styles
    
    * Update padding in EditTask styles
    
    * Remove rounded class from Button styles
    
    * Update date color in Datepicker styles
    
    * Update wrap duedate with div in Task.jsx
    
    * Update Task styles
    
    * Remove extra div in TaskInput.jsx
    
    * Udpate TaskInput styles
    
    * Update TaskMenu styles
    
    * Add sortedTasks array to sortByDate util

--- src/utils/sortByDate.js
+++ src/utils/sortByDate.js
@@ -8,6 +8,8 @@ dayjs.extend(isTomorrow);
 dayjs.extend(isBetween);
 
 const sortByDate = (selectedList, tasks) => {
+  const sortedTasks = tasks.sort((a, b) => new Date(a.date) - new Date(b.date));
+
   if (selectedList === "Today") {
     return tasks.filter((task) => dayjs(task.date).isToday());
   } else if (selectedList === "Tomorrow") {
@@ -16,19 +18,19 @@ const sortByDate = (selectedList, tasks) => {
     const startWeek = dayjs().add(1, "day");
     const endWeek = dayjs().add(7, "day");
 
-    return tasks.filter((task) =>
+    return sortedTasks.filter((task) =>
       dayjs(task.date).isBetween(startWeek, endWeek)
     );
   } else if (selectedList === "Upcoming") {
-    return tasks.filter((task) =>
+    return sortedTasks.filter((task) =>
       dayjs(task.date).isAfter(dayjs().add(7, "day"))
     );
   } else if (selectedList === "Overdue") {
-    return tasks.filter(
+    return sortedTasks.filter(
       (task) => dayjs(task.date).isBefore(dayjs(), "day") && !task.isDone
     );
   } else {
-    return tasks.filter((task) => !task.isDone);
+    return sortedTasks.filter((task) => !task.isDone);
   }
 };
 
