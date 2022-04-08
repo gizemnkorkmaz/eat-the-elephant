354cf13db2721291bc0cfc4e195ed655d517ea5b
Gizem <gizemnurkorkmaz@gmail.com>
2022-04-08 23:18:24 +0300

    Add clear date button to DatePicker.jsx

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -4,17 +4,26 @@ import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import Icon from "../Icon/Icon";
 
-const Datepicker = ({ selected, onChange }) => (
-  <label className={styles.CalendarWrapper}>
-    <Icon icon="calendar" size={16} className={styles.CalendarIcon} />
-    <DatePicker
-      selected={selected ? new Date(selected) : null}
-      onChange={onChange}
-      className={styles.Calendar}
-      minDate={new Date()}
-      dateFormat="d MMM"
-    />
-  </label>
-);
+const Datepicker = ({ onChange, task, setTask }) => {
+  const clearDate = () => {
+    setTask({ ...task, date: "" });
+  };
+  return (
+    <label className={styles.CalendarWrapper}>
+      <Icon icon="calendar" size={16} className={styles.CalendarIcon} />
+      <DatePicker
+        selected={task.date ? new Date(task.date) : undefined}
+        onChange={onChange}
+        className={styles.Calendar}
+        minDate={new Date()}
+        dateFormat="d MMM"
+      >
+        <div className={styles.ClearDate} onClick={clearDate}>
+          Clear Date
+        </div>
+      </DatePicker>
+    </label>
+  );
+};
 
 export default Datepicker;
