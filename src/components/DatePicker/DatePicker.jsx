571a91408b30f3c0662a6f74ad0d4f152ad8926f
Gizem <gizemnurkorkmaz@gmail.com>
2022-04-09 14:26:37 +0300

    Add closeOnScroll to Datepicker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -7,25 +7,25 @@ import Icon from "../Icon/Icon";
 const Datepicker = ({ onChange, task, setTask, inputRef }) => {
   const clearDate = (e) => {
     e.preventDefault();
-    inputRef.current.focus();
     setTask({ ...task, date: "" });
+
+    inputRef.current.focus();
   };
   return (
     <label className={styles.CalendarWrapper}>
       <Icon icon="calendar" size={16} className={styles.CalendarIcon} />
-      <label>
-        <DatePicker
-          selected={task.date ? new Date(task.date) : ""}
-          onChange={onChange}
-          className={styles.Calendar}
-          minDate={new Date()}
-          dateFormat="d MMM"
-        >
-          <div className={styles.ClearDate} onClick={clearDate}>
-            Clear Date
-          </div>
-        </DatePicker>
-      </label>
+      <DatePicker
+        selected={task.date ? new Date(task.date) : ""}
+        onChange={onChange}
+        className={styles.Calendar}
+        minDate={new Date()}
+        dateFormat="d MMM"
+        closeOnScroll
+      >
+        <div className={styles.ClearDate} onClick={clearDate}>
+          Clear Date
+        </div>
+      </DatePicker>
     </label>
   );
 };
