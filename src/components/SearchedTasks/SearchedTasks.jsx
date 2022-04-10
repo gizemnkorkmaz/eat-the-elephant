cc2b3662bb1976d4adb8771d507d663a5a3fd8ef
Gizem <gizemnurkorkmaz@gmail.com>
2022-04-10 19:14:25 +0300

    Add SearchedTask component

---
+++ src/components/SearchedTasks/SearchedTasks.jsx
@@ -0,0 +1,25 @@
+import styles from "./SearchedTasks.module.css";
+
+import { useContext } from "react";
+import { TasksContext } from "../../context/tasksContext";
+
+import Task from "../Task/Task";
+
+const SearchedTasks = () => {
+  const { tasks, searchedTask } = useContext(TasksContext);
+  const searchResults = tasks.filter((task) =>
+    task.name.toLowerCase().includes(searchedTask.toLowerCase())
+  );
+
+  return (
+    <div className={styles.Tasks}>
+      {searchResults.length ? (
+        searchResults.map((task) => <Task key={task.id} task={task} />)
+      ) : (
+        <div className={styles.NoTaskMessage}>No task found</div>
+      )}
+    </div>
+  );
+};
+
+export default SearchedTasks;
