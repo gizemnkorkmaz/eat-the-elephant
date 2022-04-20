6935fd8e5b86b4f8ffa7a9eaeb3b1316d8aa5fa5
Gizem <gizemnurkorkmaz@gmail.com>
2022-04-20 23:41:10 +0300

    Update SearchedTasks

--- src/components/SearchedTasks/SearchedTasks.jsx
+++ src/components/SearchedTasks/SearchedTasks.jsx
@@ -7,15 +7,17 @@ import styles from "./SearchedTasks.module.css";
 
 const SearchedTasks = ({ searchedTask }) => {
   const { tasks } = useContext(TasksContext);
+
   const searchResults = tasks.filter((task) =>
     task.name.toLowerCase().includes(searchedTask.toLowerCase())
   );
 
   return (
     <div className={styles.Tasks}>
-      {searchResults.length ? (
-        searchResults.map((task) => <Task key={task.id} task={task} />)
-      ) : (
+      {searchResults.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+      {!searchResults.length && (
         <div className={styles.NoTaskMessage}>
           No matches for "{searchedTask}"
         </div>
