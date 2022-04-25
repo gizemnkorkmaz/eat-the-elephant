43c6bc8d7a4c5fbb48be0d028bcb979e892ab127
Gizem <gizemnurkorkmaz@gmail.com>
2022-04-26 00:14:05 +0300

    Add value to search task to clear on list switching

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,21 @@ import Icon from "../Icon/Icon";
 
 import styles from "./Header.module.css";
 
-const Header = ({ setSelectedList, setSearchedTask, toggleSidebar }) => (
+const Header = ({
+  selectedList,
+  setSelectedList,
+  searchedTask,
+  setSearchedTask,
+  toggleSidebar,
+}) => (
   <header className={styles.Header}>
     <div className={styles.Menu}>
       <div className={styles.MenuIcon}>
         <Icon icon="menu" size={24} onClick={toggleSidebar} />
       </div>
       <SearchInput
+        searchedTask={searchedTask}
+        selectedList={selectedList}
         setSelectedList={setSelectedList}
         setSearchedTask={setSearchedTask}
       />
